53944dcbd33a082a7abeed229a110996e5a54c6e
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-12-04 11:17:05 +0900

    update cellular_automaton add cell_generator()

--- ex2_cellular_automaton.py
+++ ex2_cellular_automaton.py
@@ -1,3 +1,5 @@
+import random 
+
 def next_cell(cell):
     if cell[0] == "0" and cell[-1] == "1":
         cell = cell[1:len(cell)-1].replace('10', '01')
@@ -7,8 +9,6 @@ def next_cell(cell):
     return cell
 
 def get_displacement(cell, t):
-    if type(cell) == list:
-        cell = list_to_str(cell)
     while t >= 1:
         cell = next_cell(cell)
         # print(cell)
@@ -22,23 +22,17 @@ def bin_to_dec(binary_str):
     return int(binary_str, 2)
 
 def cell_generator(num):
+    cell = [random.uniform(0,1) for _ in range(num)]
+    return ''.join(cell)
 
 def main():
-    print("--"*10+"問題1"+"--"*10)
-    print("init cell : 111100011001111100110101")
-    cell1 = "111100011001111100110101"
-    print("--"*10+"答え"+"--"*10)
-    bs1 = get_displacement(cell1, 100)
-    print("10進数 : ",bin_to_dec(bs1))
-    print("2進数 : ",bs1) 
-
-    print("--"*10+"問題2"+"--"*10)
-    print("init cell : 011111111001100000110000")
-    cell2 = "011111111001100000110000"
-    print("--"*10+"答え"+"--"*10)
-    bs2 = get_displacement(cell2, 100)
-    print("10進数 : ",bin_to_dec(bs2))
-    print("2進数 : ",bs2) 
+    cell = cell_generator(100) #100マス
+    print("--"*10+"input"+"--"*10)
+    print("init cell : ", cell)
+    print("--"*10+"output"+"--"*10)
+    out_cell = get_displacement(cell, 100)
+    print("decimal : ",bin_to_dec(out_cell))
+    print("binary : ", out_cell) 
 
 if __name__ == "__main__":
     main()
