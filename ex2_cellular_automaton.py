03c87d401e734aa31185f98fd726208ca8a38f1a
Capi53 <e185753@ie.u-ryukyu.ac.jp>
2020-12-12 14:58:49 +0900

    rep3の追加とnext_cellの修正

--- ex2_cellular_automaton.py
+++ ex2_cellular_automaton.py
@@ -1,11 +1,11 @@
 import random 
+import matplotlib.pyplot as plt 
 
 def next_cell(cell):
-    if cell[0] == "0" and cell[-1] == "1":
-        cell = cell[1:len(cell)-1].replace('10', '01')
-        cell = "1" + cell + "0" 
-    else:
-        cell = cell.replace('10', '01')
+    cell = cell[-1] + cell + cell[0]
+    cell = cell.replace('10', '01')
+    cell = cell[1:-1]
+
     return cell
 
 def get_displacement(cell, t):
@@ -38,6 +38,35 @@ def is_cluster(cell):
     else:
         return False
 
+def get_flow_rate(cell, loops, target):
+    """
+    流量を調べる関数
+
+    Parameters
+    ---------
+    cell  string 
+        セル
+    loops : int
+        ループ回数
+    target : int
+        ターゲットとするindexの番号
+
+    Returns
+    -------
+    f_rate_counter : int
+        流量(0 <=x<= loops)
+    """
+    tmp_cell = cell #比較対象
+    f_rate_counter = 0 #ますで変化があった回数
+
+    while loops >= 1:
+        cell = next_cell(cell)
+        if cell[target] != tmp_cell[target]:
+            f_rate_counter += 1
+        tmp_cell = cell
+        loops -= 1
+    return f_rate_counter//2
+
 def main():
     #--rep1--
     # cell = cell_generator(100) #100マス
@@ -49,23 +78,37 @@ def main():
     # print("binary : ", out_cell) 
 
     #--rep2--
-    for i in range(1,52):
-        input_cell = cell_generator(100, i)
-        # print(input_cell)
-        if not is_cluster(input_cell):
-            out_cell = get_displacement(input_cell, 10)#だいたい10回やればクラスタができるかどうか分かるはずだ!!
-            if is_cluster(out_cell):
-                print(out_cell)
-                print("Cluster found")
-                print(i)
-                exit()
-            else:
-                print("Cluster not found")
-        else :
-            print(input_cell)
-            print("Cluster found")
-            print(i)
-            exit()
+    # for i in range(1,52):
+    #     input_cell = cell_generator(100, i)
+    #     # print(input_cell)
+    #     if not is_cluster(input_cell):
+    #         out_cell = get_displacement(input_cell, 10)#だいたい10回やればクラスタができるかどうか分かるはずだ!!
+    #         if is_cluster(out_cell):
+    #             print(out_cell)
+    #             print("Cluster found")
+    #             print(i)
+    #             exit()
+    #         else:
+    #             print("Cluster not found")
+    #     else :
+    #         print(input_cell)
+    #         print("Cluster found")
+    #         print(i)
+    #         exit()
+
+    #--rep3--
+    #流量を求めて配列に保存する．
+    flows = [0]
+    for i in range(1, 101):
+        input_cell = cell_generator(100,i)
+        flows.append(get_flow_rate(input_cell,100,1))#100ますのセルを100回動かすのでtargetもランダムで選んでもいいかも
+    x=[int(idx) for idx in range(101)]
+    plt.figure(0,(6,4))
+    plt.plot(x, flows,linestyle="solid", marker="o")
+    plt.xlabel("cells")
+    plt.ylabel("flows")
+    plt.savefig("任意のセルでの流量観測")
+
 
 
 if __name__ == "__main__":
